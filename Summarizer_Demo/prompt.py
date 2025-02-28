01747f956da43e14d756b2f8902c520340430f12
K Vishwajith <133970712+v1shwaj1th@users.noreply.github.com>
2025-02-28 11:24:05 +0530

    Add files via upload

---
+++ Summarizer_Demo/prompt.py
@@ -0,0 +1,53 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+import os
+
+import streamlit as st
+from langchain_core.prompts import PromptTemplate, load_prompt
+
+load_dotenv()
+api_key = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+
+if not api_key:
+    raise ValueError("Hugging Face API token is missing. Set it in the .env file.")
+
+llm = HuggingFaceEndpoint(
+    repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    task="text-generation",
+    huggingfacehub_api_token=api_key
+)
+
+model = ChatHuggingFace(llm=llm)
+
+st.header('Research Tool')
+
+paper_input = st.selectbox(
+    "Select Research Paper Name",
+    [
+        "Attention Is All You Need",
+        "BERT: Pre-training of Deep Bidirectional Transformers",
+        "GPT-3: Language Models are Few-Shot Learners",
+        "Diffusion Models Beat GANs on Image Synthesis"
+    ]
+)
+
+style_input = st.selectbox(
+    "Select Explanation Style",
+    ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"]
+)
+
+length_input = st.selectbox(
+    "Select Explanation Length",
+    ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"]
+)
+
+template = load_prompt('template.json')
+
+if st.button('Summarize'):
+    chain = template | model
+    result = chain.invoke({
+        'paper_input': paper_input,
+        'style_input': style_input,
+        'length_input': length_input
+    })
+    st.write(result.content)
